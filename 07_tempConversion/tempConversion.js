42e2de3ef9574530d806b0c70483778739489f89
Roy Chan <roychc1@gmail.com>
2023-07-05 02:09:23 -0700

    fundamentals part 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return (Math.round((f - 32) * (5/9) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return (Math.round(c * (9/5) * 10) / 10 + 32);
 };
 
 // Do not edit below this line
